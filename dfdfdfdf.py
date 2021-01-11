cfba7602e4d124f802002d8302aa7cca470a28b7
Qegashi <77283585+Qegashi@users.noreply.github.com>
2021-01-11 17:00:25 +0200

    Create dfdfdfdf.py
    3rd commit

---
+++ dfdfdfdf.py
@@ -0,0 +1,6 @@
+numbers = list(range(10))
+print(numbers)      # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+numbers = list(range(2, 10))
+print(numbers)      # [2, 3, 4, 5, 6, 7, 8, 9]
+numbers = list(range(10, 2, -2))
+print(numbers)      # [10, 8, 6, 4]
